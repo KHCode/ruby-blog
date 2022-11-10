1335127032337cf238618f668c40c112f6760484
Kris Hill <krish@ricksteves.com>
2022-11-09 16:07:03 -0800

    [migration] add articles to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   has_secure_password
-
+  has_many :articles, dependent :nullify
   validates_uniqueness_of :email
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_03_175725) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_09_234739) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -75,9 +75,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_03_175725) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "articles_id", null: false
+    t.index ["articles_id"], name: "index_users_on_articles_id"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "comments", "articles"
+  add_foreign_key "users", "articles", column: "articles_id"
 end
